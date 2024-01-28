9e7da0d0a02273f47fbc234ae7f5757c804a2fc5
kimseeun <cocobell3@naver.com>
2024-01-29 00:43:19 +0900

    study: id로 특정 게시물 지우기

--- src/boards/boards.service.ts
+++ src/boards/boards.service.ts
@@ -23,4 +23,7 @@ export class BoardsService {
   getBoardById(id: string): Board {
     return this.boards.find((board) => board.id === id);
   }
+  deleteBoard(id: string): void {
+    this.boards = this.boards.filter((board) => board.id != id);
+  }
 }
