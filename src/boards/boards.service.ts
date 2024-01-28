7a8900ae3a15f84d386acff771f0e20f7b567991
kimseeun <cocobell3@naver.com>
2024-01-29 00:14:33 +0900

    study: 게시물 생성을 위한 DTO

--- src/boards/boards.service.ts
+++ src/boards/boards.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { Board, BoardStatus } from './board.model';
 import { v1 as uuid } from 'uuid';
+import { CreateBoardDto } from './dto/create-board.dto';
 @Injectable()
 export class BoardsService {
   private boards: Board[] = [];
@@ -8,11 +9,12 @@ export class BoardsService {
   getAllBoards(): Board[] {
     return this.boards;
   }
-  createBoard(title: string, description: string) {
+  createBoard(createBoardDto: CreateBoardDto) {
+    const { title, description } = createBoardDto;
     const board: Board = {
       id: uuid(),
-      title: title,
-      description: description,
+      title,
+      description,
       status: BoardStatus.PUBLIC,
     };
     this.boards.push(board);
