d3384fa30f62995d0c3d2829e3153a2326b76eab
kimseeun <cocobell3@naver.com>
2024-01-28 23:06:31 +0900

    study: 모든 게시물을 가져오는 service만들기

--- src/boards/boards.service.ts
+++ src/boards/boards.service.ts
@@ -1,4 +1,10 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class BoardsService {}
+export class BoardsService {
+  private boards = ['ddd', 'ddd', 'ddd'];
+
+  getAllBoards() {
+    return this.boards;
+  }
+}
