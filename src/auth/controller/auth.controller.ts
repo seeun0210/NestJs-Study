c5d1cd5bc4a06731dd91773af3cf9aceec157951
kimseeun <cocobell3@naver.com>
2024-01-30 17:57:26 +0900

    study: entity unique속성 사용한 유효성 검사

--- src/auth/controller/auth.controller.ts
+++ src/auth/controller/auth.controller.ts
@@ -8,11 +8,10 @@ export class AuthController {
   constructor(private authService: AuthService) {}
 
   @Post('/signup')
-  signUp(
+  async signUp(
     @Body(ValidationPipe) authCredentialsDto: AuthCredentialsDto,
   ): Promise<AuthCredentialsDto> {
-    console.log(this.authService.signUp(authCredentialsDto));
-    const result = this.authService.signUp(authCredentialsDto);
+    const result = await this.authService.signUp(authCredentialsDto);
     console.log(result);
     return result;
   }
--- src/auth/user.repository.ts
+++ src/auth/user.repository.ts
@@ -2,13 +2,26 @@ import { CustomRepository } from 'src/typeorm/typeorm-ex.decorator';
 import { Repository } from 'typeorm';
 import { User } from './user.entity';
 import { AuthCredentialsDto } from './dto/auth-credential.dto';
+import {
+  ConflictException,
+  InternalServerErrorException,
+} from '@nestjs/common';
 @CustomRepository(User)
 export class UserRepository extends Repository<User> {
   async createUser(
     authCredentialsDto: AuthCredentialsDto,
-  ): Promise<AuthCredentialsDto> {
+  ): Promise<User | null> {
     const { username, password } = authCredentialsDto;
     const newUser = this.create({ username, password });
-    return await this.save(newUser);
+    try {
+      return await this.save(newUser);
+    } catch (err) {
+      console.log('err::', err);
+      if (err.code === '23505') {
+        throw new ConflictException('이미 존재하는 이름입니다.');
+      } else {
+        throw new InternalServerErrorException();
+      }
+    }
   }
 }
